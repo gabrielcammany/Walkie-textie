570a6971e0f74e9ce8df3fe29eee3b078c64d36d
Gabriel Cammany <gabriel.cammany@gmail.com>
2017-04-11 19:11:51 +0200

    Both Pseudocode and Assembler code are done. Need to check if there are any bugs.

--- AssemblerCode/PseudoCode.c
+++ AssemblerCode/PseudoCode.c
@@ -1,13 +1,14 @@
-#define FLAG_ENVIAR_RF_MSG 0x80
 #define POLS_CARREGA 0x05
 #define POLS_ENVIA_RF 0x04
 #define BIT_RESPOSTA_PC 0x08
 #define FLAG_DESAR_MSG 0x81
 #define FLAG_ENVIAR_RF_MSG 0x82
 #define FLAG_CONFIRMACIO_MSG 0x83
+#define FLAG_DESAR_SENSE_CONFIRMACIO_MSG 0x84
 #define ESTAT_LEDS_CIRCULAR 0x04
 #define ESTAT_POLSADOR 0x01
 #define ESTAT_BLINKING_10HZ 0x08
+#define ESTAT_BLINKING_5HZ 0x09
 #define POSICIO_A_DESAR_RAM 0x80
 #define END_BYTE 0x88
 #define MAX_LEDS 0x0B
@@ -22,7 +23,7 @@
 #define LED8 0x04
 #define LED9 0x05
 
-int temps = 0x00, comptaBytes = 0, ESTAT = 0, aux = 0x00, LEDs = 0x01;
+int comptaBytes = 0, ESTAT = 0, aux = 0x00, LEDs = 0x01;
 
 
 void main(){
@@ -36,60 +37,93 @@ void main(){
 		if(LATC.POLS_ENVIA_RF == 1){
 			pols_enviar_rf();
 		}
-		if(ESTAT == ESTAT_LEDS_CIRCULAR){ //Leds circular
+		if(ESTAT == ESTAT_LEDS_CIRCULAR){
 			ledsCircular();
 		}
 		if(ESTAT == ESTAT_POLSADOR){
 			espera_polsador();
 		}
-		if(status == ESTAT_BLINKING_10HZ){ //Blinking 10 Hz
+		if(status == ESTAT_BLINKING_10HZ){
 			blinking_10hz();
 		}
 	}
 }
 
-void blinking_10hz(){
-	if(tempsUn == 20){
-				encendreLEDS(); //Encendre i apagar els leds
-	}
-	if(tempsUn == 40){
-		apagarLEDS();
-		tempsUn = 0x00;
-	}
-}
-
+//Fet
 void espera_polsador(){
-	if(tempsUn >= 10){
-				tempsUn = 0x00;
-				status = 0x04;
+	if(tempsUn >= 200){
+		auxiliar++;
+		if(auxiliar >= 50){
+			activar_leds_circular();
+		}
 	}
 }
 
+//Fet
 void pols_carrega_missatge(){
-	status = 0x01;
-	tempsUn = 0x00;
-	enviaPC(0x91);
+	status = ESTAT_POLSADOR;
+	tempsUn = 0;
+	FLAG_DESAR_SENSE_CONFIRMACIO_MSG -> TXREG;
+}
+//Fet
+void pols_enviar_rf(){
+	enviaRF();
 }
 
-void pols_enviar_rf(){
-	if(comptaBytes != 0x00){
-		enviaRF(comptaBytes);
-	}else{
-		LEDs = 0x20;
-		tempsUn = 0x00;
-		status = 0x04;
+//Fet
+void enviarConfirmacioRf(){
+	FLAG_ENVIAR_RF_MSG -> TXREG
+}
+
+//Fet
+void enviarConfirmacioDesar(){
+	FLAG_DESAR_MSG -> TXREG
+}
+
+//Fet
+void blinking_10hz(){
+	if(tempsUn == 0x14){
+		encendreLEDS(); //Encendre i apagar els leds
+	}
+	if(tempsUn == 0x28){
+		apagarLEDS();
 	}
 }
 
+//Fet
+void blinking_5hz(){
+	if(tempsUn == 0x28){
+		encendreLEDS(); //Encendre i apagar els leds
+	}
+	if(tempsUn == 0x50){
+		apagarLEDS();
+	}
+}
+
+//Fet
+void encendreLEDS(){
+	0xFF -> LATB;
+	0xFF -> LATD;
+}
+
+//Fet
+void apagarLEDS(){
+	0xC0 -> LATB;
+	0xF0 -> LATD;
+	tempsUn = 0;
+}
+
+//Fet
 void rebut(){
 	tempsUn = 0x00;
 	if(RCREG.BIT_RESPOSTA_PC == 1){
-		if(RCREG < FLAG_ENVIAR_RF_MSG){ 
-			desar()
+		if(RCREG < FLAG_DESAR_MSG){ 
+			desar();
 		}
-		if(RCREG == FLAG_CONFIRMACIO_MSG){ 
+		if(RCREG == FLAG_ENVIAR_RF_MSG){ 
 			enviaRF();
-		}else{
+		}
+		if(RCREG == FLAG_SENSE_CONFIRMACIO_MSG){
 			desar_sense_confirmacio();
 		}
 	}else{
@@ -97,102 +131,135 @@ void rebut(){
 	}
 }
 
+//Fet
 void HighInt(){
 	resetTimer();
 	tempsUn++;
 }
 
-void ledsCircular(){
-	//Per fer
-	
-}
-
-void encendreLEDS(){
-	0xFF -> LATB;
-	0xFF -> LATD;
-}
-
-void apagarLEDS(){
-	0xC0 -> LATB;
-	0xF0 -> LATD;
-}
-
-
-
-void enviaRF(int num){
+//Fet
+void enviaRF(){
 	if(comptaBytes != 0x00){
 		LATC = 0x00;
 		LATD = 0x00;
 		LEDs = 0x01; //Ja que es del 1-10
 		enviaConfirmacio();
-		result = dividir10(num);
+		result = dividir10();
 		char i = 0x00;
 		while(LEDs < MAX_LEDS){ //Hi hauran 10 leds i anira 10-1
 			while(i<result){ // Result es el numero de bytes total entre 10
 				POSTINC0 -> AUX; //Afegim el valor de la ram al auxiliar
-				tempsUn = 0x00; // Per tal de fer el temps a 0 i 1 amb la codificacio manchester, fiquem tempsUn a 0 per quan salti la interrupcio
+				tempsUn = 0x00; // Per tal de fer el temps a 0 i 1 amb la codificacio manchester, fiquem 
+								//tempsUn a 0 per quan salti la interrupcio
 				while(enviat<0x08){ //Enviat son els 8 bits que hem denviar
 					if(tempsUn == 0x01){
-						enviar_bit_pos();
+						enviar_bit_pos(); //Primers 5 mseg
 					}
 					if(tempsUn == 0x02){
-						enviar_bit_neg();
-						AUX >> 1;
-						tempsUn = 0x00;
+						enviar_bit_neg(); //Segon 5 mseg
+						AUX >> 1; //Posem la seguent dada 
+						tempsUn = 0x00; //Reiniciem el comptador
 					}
 				}
 				i++;
 			}
+			i = 0;
 			LEDs++; 
 			activar_leds();
 		}
+		ESTAT = ESTAT_BLINKING_10HZ;
 	}else{
-		//LEDs circular
+		activacio_leds_circular();
 	}
 }
 
-void enviar_bit_pos(){
+//Fet
+void ENVIAR_BIT_PRIMERA_MEITAT(){
 	if(AUX.Bit0){ //Si el bit a enviar es 1
 		clrBit(LATC5.Bit5); //La primera part a 0
 	}else{
-		setBit(LATC5.Bit5); //La primera part a 0
+		setBit(LATC5.Bit5); //La primera part a 1
 	}
 }
 
-void enviar_bit_neg(){
+//Fet
+void ENVIAR_BIT_SEGONA_MEITAT(){
 	if(AUX.Bit0){ //Si el bit a enviar es 1
-		setBit(LATC5.Bit5); //La segona part a 1
+		setBit(LATC.Bit5); //La segona part a 1
 	}else{
-		clrBit(LATC5.Bit5); //La segona part a 1
+		clrBit(LATC.Bit5); //La segona part a 0
 	}
-	enviat++;//Hem de tenir un recompte dels bits enviats per anar a la seguent dada en la ram un cop enviats 8 
-	
+	enviat++;//Hem de tenir un recompte dels bits enviats per anar a 
+			 //la seguent dada en la ram un cop enviats 8 
 }
 
-void activar_leds(){
+//Fet
+void activar_leds(){ 
 	if(LEDs > 1){
 		if(!LATD.LED3){
 			LATD >> 1;
 			LATD.LED0 = 1;
 		}else{
-			LATC >> 1;
-		}
-		if(STATUS.C){
-			LATC.LED4 = 1;
+			LATB >> 1;
+			LATB.LED4 = 1;
 		}
 	}else{
 		LATD.LED0
 	}
 }
 
+//Fet
+void activacio_leds_circular(){
+	ESTAT = ESTAT_LEDS_CIRCULAR;
+	LATB = 0;
+	LATD = 0;
+	tempsUn = 0;
+	index = 1;
+}
+
+//Fet
+void activar_leds_circular(){ 
+	if(tempsUn > 100){
+		if(dreta == 1){
+			if(LATD > 0){
+				LATD >> 1;
+				LATD.Bit0 = 0;
+				if(STATUS.C){
+					LATD = 0;
+					LATB.LED4 = 1;
+				}
+			}else{
+				LATB >> 1;
+				if(LATD.Bit6){
+					dreta = 0;
+					LATB.LED9 = 1;
+				}
+			}
+		}else{
+			if(LATD > 0){
+				LATD << 1;
+				LATD.LED3 = 0;
+				if(LATD.Bit3){
+					LATD.LED0 = 1;
+					dreta = 1;
+				}
+			}else{
+				LATB << 1;
+				if(STATUS.C){
+					LATD.LED3 = 1;
+				}
+			}
+		}
+	}
+}
+
+//Fet
 void dividir10(int num){
 	num = ((num * 205).High); //Donat que 205/2048 dona 0,10009765, es una aproximacio a 0,10. Ens permet dividir qualsevol numero de 0-300 entre 10 amb un error d'un bit sense us de taules.
 	(num >> 3); //El número maxim sera ‭1111 0000 0011 1100 (300d) ‬Agafarem el byte high i shiftarem 3 posicions es a dir: 1111 0000 (240d) -> 0001 1111 (30d) (Ja que 240/8 -> 30)
 }
 
-void enviaPeticioMissatge(){
-}
-
+//Fet
 void desar(){
 	enviaConfirmacio();
 	comptaBytes = 0;
@@ -201,15 +268,18 @@ void desar(){
 	
 }
 
+//Fet
 void bucle_desar(){
-	if(PIR1.RCIF == 0){goto bucle_desar();}
-	if(RCREG == END_BYTE){
+	if(PIR1.RCIF == 0){goto bucle_desar();} //Mentres no hem rebut res esperar
+	if(RCREG == END_BYTE){ //Quan el ordinador ens envii el end byte pararem
+		ESTAT = ESTAT_BLINKING_5HZ;
 		return //Retornara al bucle principal ja que hem vingut a desar amb goto
 	}
 	POSTINC0 = RCREG;
 	goto bucle_desar();
 }
 
+//Fet
 void desar_sense_confirmacio(){
 	comptaBytes = 0;
 	FSR0 = POSICIO_A_DESAR_RAM;
--- AssemblerCode/main.asm
+++ AssemblerCode/main.asm
@@ -13,24 +13,42 @@ CONFIG	WDT = OFF
 ;*************
     
 ESTAT ORG 0x02   
-TEMPS_UN ORG 0x03     
+TEMPS_UN ORG 0x03    
+COMPTA_BYTES ORG 0x04 
+LEDs ORG 0x04  
+RESULTAT_DIVISIO ORG 0x05
+INDEX ORG 0x06   
+AUXILIAR ORG 0x07   
+ENVIAT ORG 0x08  
 
  
 ;*************
 ;* CONSTANTS *
 ;*************
+ZERO ORG 0x00
 POLS_CARREGA ORG 0x05   
 POLS_ENVIA_RF ORG 0x04 
 BIT_RESPOSTA_PC ORG 0x08   
 FLAG_DESAR_MSG ORG 0x81
 FLAG_ENVIAR_RF_MSG ORG 0x82 
 FLAG_CONFIRMACIO_MSG ORG 0x83
+FLAG_DESAR_SENSE_CONFIRMACIO_MSG ORG 0x84
 ESTAT_LEDS_CIRCULAR ORG 0x04
 ESTAT_POLSADOR ORG 0x01
 ESTAT_BLINKING_10HZ ORG 0x08
+ESTAT_BLINKING_5HZ ORG 0x09
 POSICIO_A_DESAR_RAM ORG 0x80
 END_BYTE ORG 0x88
 MAX_LEDS ORG 0x0B
+ 
+VALOR_A_MULTIPLICAR ORG 0xCD //205
+ 
+TEMPS_100_MSEG ORG 0x14 //20
+TEMPS_200_MSEG ORG 0x28 //40
+TEMPS_400_MSEG ORG 0x50 //80
+TEMPS_500_MSEG ORG 0x64 //100
+TEMPS_1000_MSEG ORG 0xC8 //200
+ 
 LED0 ORG 0x04
 LED1 ORG 0x05
 LED2 ORG 0x06
@@ -40,8 +58,8 @@ LED5 ORG 0x01
 LED6 ORG 0x02
 LED7 ORG 0x03
 LED8 ORG 0x04
-LED9 ORG 0x05
-
+LED9 ORG 0x05 
+ 
 ;*********************************
 ; VECTORS DE RESET I INTERRUPCIï¿½ *
 ;*********************************
@@ -169,30 +187,122 @@ LOOP
     btfsc ESTAT,ESTAT_BLINKING_10HZ,0
     call BLINKING_10HZ
     
+    btfsc ESTAT,ESTAT_BLINKING_5HZ,0
+    call BLINKING_5HZ
+    
     goto LOOP
+
+//**************** - BLOC RF - ****************************
     
 ENVIAR_RF
-      
+    movlw ZERO
+    cpfsgt COMPTA_BYTES, 0
+    goto ACTIVACIO_LEDS_CIRCULAR //Activar leds circular
+    clrf LATB, 0
+    clrf LATD, 0
+    clrf LEDs, 0
+    clrf AUXILIAR,0
+    incf LEDs, 1, 0 
+    call ENVIAR_CONFIRMACIO_RF
+    call DIVIDIR_10
+    clrf INDEX,0
+
+BUCLE_LEDS_ENVIAR_RF
+    movlw MAX_LEDS
+    cpfslt LEDs
+    goto FINAL_RF
+    movf RESULTAT_DIVISIO,0,0
+
+BUCLE_INDEX_DIVISOR_ENVIAR_RF 
+    cpfslt INDEX,0 //Mentre INDEX < RESULTAT_DIVISIO ens quedem aqui
+    goto FINAL_BUCLE_INDEX_DIVISOR //Quan sigui igual, incrementarem variable leds i activarem leds.
+    movff POSTINC0, AUXILIAR
+    clrf TEMPS_UN,0
+    
+BUCLE_ENVIAR_8_BITS //Ens quedarem aqui fins que no hem enviat 8 bits
+    movlw 0x08 
+    cpfslt ENVIAT,0
+    goto FINAL_BUCLE_ENVIAR //Incrementarem index
+    btfsc TEMPS_UN, 0,0 
+    goto ENVIAR_BIT_PRIMERA_MEITAT //Primers 5 mseg
+    btfsc TEMPS_UN, 1,0
+    goto ENVIAR_BIT_SEGONA_MEITAT //Segon 5 mseg
+    goto BUCLE_ENVIAR_8_BITS //Tornem al inici del bucle   
+    
+FINAL_BUCLE_ENVIAR 
+    incf INDEX, 0,0
+    goto BUCLE_INDEX_DIVISOR_ENVIAR_RF //Tornem al seguent 10 percent
+    
+FINAL_BUCLE_INDEX_DIVISOR
+    incf LEDs,0,0
+    call ACTIVAR_LEDS_PROCES
+    clrf INDEX, 0
+    goto BUCLE_LEDS_ENVIAR_RF
+
+ENVIAR_BIT_PRIMERA_MEITAT
+    btfsc AUXILIAR,0,0 //Mirem si el primer bit es 0, si ho es fiquem la primera part a 0
+    bcf LATC, 5, 0
+    btfss AUXILIAR,0,0 //Si el primer es 0 fiquem la primera part a 1 
+    bsf LATC, 5, 0
+    goto BUCLE_ENVIAR_8_BITS
+    
+ENVIAR_BIT_SEGONA_MEITAT
+    btfsc AUXILIAR,0,0 //Mirem si el primer bit es 0, si ho es fiquem la segona part a 1
+    bsf LATC, 5, 0
+    btfss AUXILIAR,0,0 //Si el primer es 1 fiquem la segona part a 0
+    bcf LATC, 5, 0
+    goto BUCLE_ENVIAR_8_BITS
+    
+ACTIVAR_LEDS_PROCES    
+    movlw 0x01
+    cpfseq LEDs,0
+    goto LEDS_PROCES_MAJOR_1
+    bsf LATD, LED0, 0
+    return
+
+LEDS_PROCES_MAJOR_1
+    btfss LATD, LED3,0
+    goto LEDS_PROCES_LATB
+    rrncf LATD, 1,0
+    bsf LATD, LED4, 0
+    return 
+
+LEDS_PROCES_LATB
+    rrncf LATB, 1,0
+    bsf LATB, LED0, 0
+    return
+
+FINAL_RF
+    movlw ESTAT_BLINKING_10HZ 
+    movwf ESTAT,0
+    clrf TEMPS_UN,0
+    return
+    
+    
+//***********************************************************   
     
 REBUT
     clrf TEMPS_UN
     btfsc RCREG, BIT_RESPOSTA_PC,0 //Qualsevol byte rebut que tingui aquest bit a 1 es refereix a una resposta del pc
     return //Si rebem un byte i no esta activat no hauriem dentrar aqui
-    movlw FLAG_ENVIAR_RF_MSG //Valor del RCREG per enviar per RF
-    cpfslt RCREG,0
+    movlw FLAG_DESAR_MSG //Valor del RCREG per enviar per RF
+    cpfsgt RCREG,0 //Si es mes gran no desara el missatge
     goto DESA //Desem els bytes
-    movlw FLAG_CONFIRMACIO_MSG //Valor del RCREG com a confirmacio del PC per enviar dades
+    movlw FLAG_ENVIAR_RF_MSG //Valor del RCREG com a confirmacio del PC per enviar dades
     cpfseq RCREG,0
     goto ENVIAR_RF //Enviar dades per RF
+    movlw FLAG_DESAR_SENSE_CONFIRMACIO_MSG //Valor del RCREG com a confirmacio del PC per enviar dades
+    cpfseq RCREG,0
     goto DESA_SENSE_CONFIRMACIO //Desar dades sense enviar confirmacio al PC ja que ja l'hem rebut
     return
  
+//**************** - BLOC DESAR - ****************************
 	
 DESA
-    call ENVIAR_CONFIRMACIO
+    call ENVIAR_CONFIRMACIO_DESAR
 
-DESA_SENSE_CONFIRMACIO
-    clrf COMPTA_BYTES;
+DESA_SENSE_CONFIRMACIO //Quan ens apreten el boto no necessitem enviar al pc la confirmacio
+    clrf COMPTA_BYTES; 
     movlw POSICIO_A_DESAR_RAM
     movwf FSR0,0
     
@@ -201,12 +311,201 @@ BUCLE_DESAR
 	goto BUCLE_DESAR //Mentres no valgui 1 el bit RCIF que ens indica que hi ha un byte ens esperem
     movlw END_BYTE 
     cpfslt RCREG, 0 //Si rebem el byte de final del ordinador sortim, no el desem
-	return
+	goto RETORNA_DESAR 
     movff RCREG, POSTINC0,0 //Movem el caracter a la posicio de la ram corresponent
     incf COMPTA_BYTES //Incrementem en numero de bytes rebut
     goto BUCLE_DESAR //Esperem una nova dada
     
+RETORNA_DESAR
+    movlw ESTAT_BLINKING_5HZ 
+    clrf TEMPS_UN,0
+    movwf ESTAT,0
+    return
     
+//***********************************************************    
+    
+
+//*********** - BLOC COMUNICACIO PC - *****************
+    
+ENVIAR_CONFIRMACIO_RF 
+    movlw FLAG_ENVIAR_RF_MSG
+    movwf TXREG,0
+    return
+    
+ENVIAR_CONFIRMACIO_DESAR
+    movlw FLAG_DESAR_MSG
+    movwf TXREG,0
+    return
+    
+ENVIAR_PETICIO_DESAR
+    clrf AUXILIAR
+    clrf TEMPS_UN
+    movlw FLAG_DESAR_SENSE_CONFIRMACIO_MSG
+    movwf TXREG,0
+    movlw ESTAT_POLSADOR,0
+    movwf ESTAT,0
+    return
+    
+//***********************************************************    
+
+
+//***************** - BLOC DIVIDIR - ************************
+    
+DIVIDIR_10
+    movlw VALOR_A_MULTIPLICAR
+    mulwf COMPTA_BYTES, 0
+    movff PRODH, RESULTAT_DIVISIO
+    rrncf RESULTAT_DIVISIO,1
+    rrncf RESULTAT_DIVISIO,1
+    rrncf RESULTAT_DIVISIO,1
+    movlw 0x1F //Assegurarnos que els bits que hem afegit son 0
+    andwf RESULTAT_DIVISIO,1,0
+    return
+    
+//***********************************************************  
+    
+
+//***************** - BLOC LEDS - ***************************
+    
+
+ACTIVACIO_LEDS_CIRCULAR
+    movlw ESTAT_LEDS_CIRCULAR
+    movwf ESTAT,0
+    clrf LATB,0
+    clrf LATD,0
+    clrf TEMPS_UN
+    clrf INDEX,0
+    bsf INDEX, 0,0
+    return
+    
+LEDS_CIRCULAR
+   movlw TEMPS_500_MSEG
+   cpfslt TEMPS_UN
+   goto ACTIVAR_LEDS_CIRCULAR
+   return
+ 
+ACTIVAR_LEDS_CIRCULAR
+   btfss INDEX,0,0 //Comprovem si estem dreta o no
+   goto LEDS_CIRCULAR_ESQUERRA
+   
+LEDS_CIRCULAR_DRETA
+   movlw 0x00
+   cpfseq LATD,0 //Comprovem que latd no estigui a 0 
+   goto LEDS_CIRCULAR_DRETA_LATB //Si ho esta hem de canviar de registre
+   rrcf LATD,0 //Rotem el LATD cap a la dreta
+   bcf LATD, 0, 0
+   btfsc STATUS, C,0 //Si ja tenim carry del primer registre hem danar al seguent
+   goto LEDS_CIRCULAR_DRETA_SEGONA_PART
+   return
+   
+LEDS_CIRCULAR_DRETA_SEGONA_PART
+    clrf LATD,0 //Borrem LATD
+    bsf LATB, LED4, 0 //Activem el primer bit del LATB
+    return //Tornem al loop principal
+
+LEDS_CIRCULAR_DRETA_LATB
+    rrcf LATB,0 
+    btfsc STATUS, C,0 //Si ja hem arribat al final ja no podem anar mes cap a la dreta
+    goto LEDS_CIRCULAR_DRETA_ESQUERRA //Haurem de canviar de sentit
+    return //Tornem al loop principal
+    
+LEDS_CIRCULAR_DRETA_ESQUERRA
+    clrf INDEX,0 //Borrem index per decidir que anirem cap a lesquerra
+    bsf LATB, LED9, 0 //Activem lultim bit de latb 
+    return //Tornem al bucle principal
+        
+    
+LEDS_CIRCULAR_ESQUERRA
+   movlw 0x00
+   cpfseq LATD,0
+   goto LEDS_CIRCULAR_ESQUERRA_LATB
+   rlcf LATD,0
+   bcf LATD, LED3, 0
+   btfsc LATD, 3,0
+   goto LEDS_CIRCULAR_ESQUERRA_DRETA
+   return
+   
+LEDS_CIRCULAR_ESQUERRA_DRETA
+    bsf INDEX,0,0 //Borrem index per decidir que anirem cap a lesquerra
+    bsf LATD, LED0, 0 //Activem lultim bit de latb 
+    return //Tornem al bucle principal
+   
+LEDS_CIRCULAR_ESQUERRA_LATB
+   rlcf LATB,0
+   btfsc STATUS,C,0
+   bsf LATD, LED3,0
+   return
+   
+LEDS_CIRCULAR_ESQUERRA_SEGONA_PART
+    clrf LATD,0
+    bsf LATB, LED4, 0
+    return
+   
+BLINKING_10HZ
+    movlw TEMPS_100_MSEG
+    cpfseq TEMPS_UN,0 //Si estem als 100 no els encendrem
+    goto COMPROVA_APAGAR_10HZ //En cas de estar per sota o sobre entrarem aqui
+    goto ENCENDRE_LEDS
+    return
+   
+COMPROVA_APAGAR_10HZ
+    cpfslt TEMPS_200_MSEG //Si estem en menys de 200 no farem res, sino els apagarem
+    goto APAGAR_LEDS
+    return
+    
+BLINKING_5HZ
+    movlw TEMPS_200_MSEG
+    cpfseq TEMPS_UN,0 //Si encara no estem als 200 mseg no els encendrem
+    goto COMPROVA_APAGAR_5HZ //Si estem per sobre o sota entrarem aqui
+    goto ENCENDRE_LEDS
+    return
+
+COMPROVA_APAGAR_5HZ
+    cpfslt TEMPS_400_MSEG //En cas de arribar ja als 400 els apagarem
+    goto APAGAR_LEDS
+    return
+  
+ENCENDRE_LEDS
+    movlw 0x3F
+    setf LATB,0
+    movlw 0x0F
+    addwf LATd,0
+    return
+    
+APAGAR_LEDS
+    clrf TEMPS_UN
+    clrf LATB,0
+    clrf LATD,0
+    return
+    
+//***********************************************************  
+    
+//***************** - BLOC POLSADORS - ***************************
+    
+POLS_CARREGA_MISSATGE
+    goto ENVIAR_PETICIO_DESAR
+    
+POLS_ENVIAR_RF
+    goto ENVIAR_RF
+
+ESPERA_POLSADOR
+    movlw TEMPS_1000_MSEG
+    cpfslt TEMPS_UN
+    goto COMPROVACIO_10_SEG
+    return
+    
+COMPROVACIO_10_SEG
+    incf AUXILIAR,1,0
+    movlw 0x32 //50 ja que 50*200 son 10k
+    cpfslt AUXILIAR,0 //Si encara no hem arribat a 50 encara ens hem desperar
+    call ACTIVACIO_LEDS_CIRCULAR
+    return
+    
+INCREMENTAR_SENSE_LEDS
+    incf AUXILIAR,1,0
+    return
+    
+//***********************************************************  
     
 ;*******
 ;* END *
